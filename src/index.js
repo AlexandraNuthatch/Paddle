5274a79983b79bf01a72a05e1ae7c198b2cffdf5
Alexandra <57107044+AlexandraNuthatch@users.noreply.github.com>
2020-07-09 20:17:24 +0200

    ball shows

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from "/src/paddle";
 import InputHandler from "/src/input";
+import Ball from "/src/ball";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -8,6 +9,7 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
 
 new InputHandler(paddle);
 
@@ -20,6 +22,8 @@ function gameLoop(timestamp) {
   ctx.clearRect(0, 0, 800, 600);
   paddle.update(deltaTime);
   paddle.draw(ctx);
+
+  ball.draw(ctx);
 
   requestAnimationFrame(gameLoop);
 }
